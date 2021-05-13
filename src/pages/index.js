afb83ecf63c67786107994db9f15d35f2c565858
mikeisfake <mikescooper@icloud.com>
2021-05-12 20:28:28 -0400

    begin setting up scss modules and global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import * as React from "react"
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <div id="hero">
-      <h1>i'm mike</h1>
-      <p>& I love to make the web beautiful</p>
-    </div>
-  </Layout>
-)
+const Index = () => {
+  return (
+    <Layout>
+      <div id="hero">
+        <h1>i'm mike</h1>
+        <p>& I love to make the web beautiful</p>
+      </div>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export default Index
