ab3731f31632642b85588f029915cdbb7d152b4e
mikeisfake <mikescooper@icloud.com>
2021-05-14 17:05:06 -0400

    add image to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Helmet} from 'react-helmet'
 
+import { StaticImage } from 'gatsby-plugin-image'
+
 import Layout from '../components/layout'
 
 const About = () => {
@@ -11,7 +13,7 @@ const About = () => {
       </Helmet>
       <div id="about">
         <section>
-          <h1>a bit about me<span>.</span></h1>
+          <h1><StaticImage src='../images/animoji.png' alt='animoji protrait' /> a bit about me<span>.</span></h1>
           <p className="description">
             My name is Mike. I am a frontend software engineer based in the DMV area. I have been writing HTML, CSS and Javascript pages as a hobby since I was 12 years old... I had a really cool myspace, and I created a ton of themes for tumblr back in the day too!. <br /><br />
             Now, 10 years on, I have decided to make my hobby into a career. I have formalized my casual experience into an education at Flatiron School, where I expanded on my foundational knowledge and acquired a deeper love and understanding of software engineering.
