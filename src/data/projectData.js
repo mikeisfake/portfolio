d22f255ce77de879f76b4ca4a25c0c4cce952fa0
mikeisfake <mikescooper@icloud.com>
2021-05-14 20:44:44 -0400

    stub out data array

---
+++ src/data/projectData.js
@@ -0,0 +1,24 @@
+export const projectData = [
+  {
+    title: 'filmr',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    github: 'https://github.com/mikeisfake/filmr',
+    image: '',
+    technologies: [
+      'ruby on rails',
+      'javascript',
+      'scss',
+      'HTML',
+    ]
+  },
+
+  {
+    title: 'thirtyfive',
+    description: '',
+    technologies: [
+      'react',
+      'redux',
+      'css'
+    ]
+  },
+]
