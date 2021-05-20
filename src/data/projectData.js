27372c7dd920a54d57bf6cb38ca642f9ace94992
mikeisfake <mikescooper@icloud.com>
2021-05-20 01:06:05 -0400

    finish adding all data for projects

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,27 +1,46 @@
 export const ProjectData = [
   {
-    title: 'filmr',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'FILMr',
+    description: 'FILMr is a social application designed for film lovers built with Ruby on Rails. I also created totally custom styles with Sass. No CSS libraries needed. It queries a third party API to return JSON data about various films a user may search for. It then allows users to create reviews for those films and reviews written by other users. I used a ruby gem call acts-as-followers to allow users to follow each other. This is done with a polymorphic association on the backend.',
     github: 'https://github.com/mikeisfake/filmr',
-    image: '',
     video: "https://www.youtube.com/embed/se5ysRHcb_A",
     technologies: [
-      'ruby on rails',
-      'javascript',
-      'scss',
-      'HTML',
+      'Ruby on Rails','jQuery','Sass','HTML'
     ]
   },
 
   {
-    title: 'thirtyfive',
-    description: '',
-    github: '',
-    image: '',
-    technologies: [
-      'react',
-      'redux',
-      'css'
+    title: 'THIRTYFIVE',
+    description: 'THIRTYFIVE is an application for forgetful folm lovers built using React and Redux for the frontend and Ruby on Rails for the backend. This app employs the use of a third party API to fetch data about movies as well as a custom Rails API for managing application data.',
+    github: 'https://github.com/mikeisfake/thirtyfive',
+    video: 'https://www.youtube.com/embed/plAryFUoAfM',
+    technologies: ['React','Redux','CSS'
     ]
   },
+
+  {
+    title: 'm_movies',
+    description: 'Another movies and reviews app with basic CRUD actions. Created with Sinatra and styled with vanilla CSS',
+    github: 'https://github.com/mikeisfake/mikes_movies',
+    video: 'https://www.youtube.com/embed/Kjyp47OqbOc',
+    technologies: ['Ruby', 'Sinatra', 'CSS']
+  },
+
+  {
+   title: 'Vocitates',
+   description: 'Vocitates is a tumblr page I created to store and share the tumblr themes I have created. It was written in HTML, CSS and a little bit of JavaScript. ',
+   github: 'https://github.com/mikeisfake/vocitates',
+   video: 'https://www.youtube.com/embed/WOs5EnHEDV8',
+   technologies: ['HTML','CSS', 'JavaScript', 'jQuery']
+ },
 ]
+
+// Data Structure
+//
+// {
+//   title: '',
+//   description: '',
+//   github: '',
+//   video: '',
+//   technologies: []
+// },
