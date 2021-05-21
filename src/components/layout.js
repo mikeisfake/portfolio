b077f6e98908b2549bc1583f1c81c2acd053abad
mikeisfake <mikescooper@icloud.com>
2021-05-21 11:00:42 -0400

    remove blog page and link

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ const Layout = ({children}) => {
     <div id="main-container">
       <Helmet>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.11.0/devicon.min.css"></link>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
       </Helmet>
       <div id="nav">
         <AniLink cover bg="#2f968a" direction="right" to='/' activeStyle={{
@@ -22,9 +23,6 @@ const Layout = ({children}) => {
         <AniLink cover bg="#2f968a" direction="right" to='/about' activeStyle={{
           'border-right' : '5px solid #2f968a'
         }}>about</AniLink>
-        <AniLink cover bg="#2f968a" direction="right" to='/blog' activeStyle={{
-            'border-right' : '5px solid #2f968a'
-        }}>blog</AniLink>
         <AniLink cover bg="#2f968a" direction="right" to='/contact' activeStyle={{
           'border-right' : '5px solid #2f968a'
         }}>contact</AniLink>
@@ -34,11 +32,14 @@ const Layout = ({children}) => {
         <div className="socials">
           <ul>
             <li>
-              <a href='https://github.com/mikeisfake'><i class="devicon-github-original"></i>
+              <a href='https://github.com/mikeisfake' target="_blank" rel="noreferrer"><i class="devicon-github-original" ></i>
               </a>
             </li>
             <li>
-              <a href='https://www.linkedin.com/in/michael-cooper-259985105/'><i class="devicon-linkedin-plain"></i> </a>
+              <a href='https://www.linkedin.com/in/michael-cooper-259985105/' target="_blank" rel="noreferrer"><i class="devicon-linkedin-plain"></i> </a>
+            </li>
+            <li>
+              <a href='https://vocitates-blog.tumblr.com/' target="_blank" rel="noreferrer"><i class="fab fa-tumblr fa-fw"></i> </a>
             </li>
           </ul>
         </div>
