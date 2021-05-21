249940908f32e55b92d9ce5fdf7e24e4abf7203c
mikeisfake <mikescooper@icloud.com>
2021-05-21 10:59:40 -0400

    clean up formatting of project cards and page

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -13,14 +13,16 @@ const ProjectCard = props => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.11.0/devicon.min.css"></link>
       </Helmet>
       <div className="card" >
-        <h2>{props.title}</h2>
         <div className="card_content">
           {props.video && iframe}
           <section className="card_details">
-            <p>{props.description}</p>
             <div>
-              <a href={props.github}><i class="devicon-github-original"></i> on GitHub</a>
+              <h2>{props.title}</h2>
+              <p>{props.description}</p>
+            </div>
+            <div>
               <TechStack technologies={props.technologies} />
+              <a href={props.github}><i class="devicon-github-original"></i> on GitHub</a>
             </div>
           </section>
         </div>
