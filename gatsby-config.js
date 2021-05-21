3ae67c29b69defdba74ddd0108427117784d70fe
mikeisfake <mikescooper@icloud.com>
2021-05-21 16:33:43 -0400

    remove gatsbt starter manifest from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,18 +17,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        
-      },
-    },
+    
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-transition-link`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
