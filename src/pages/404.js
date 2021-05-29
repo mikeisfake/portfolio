5374a861376fa4ae32fb8466ae5591768232c0ce
mikeisfake <mikescooper@icloud.com>
2021-05-29 19:48:01 -0400

    customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,19 @@
 import * as React from "react"
 
+import { Emoji } from '../components/Emoji.js'
+import Layout from '../components/Layout'
+
 const NotFoundPage = () => (
-<> </>
+<Layout>
+  <div id="hero">
+    <div className="title">
+      <h2>
+        there's nothing here... &nbsp;
+        <Emoji hex='&#x1F914;' description="thinking face emoji"/>
+      </h2>
+    </div>
+  </div>
+</Layout>
 )
 
 export default NotFoundPage
