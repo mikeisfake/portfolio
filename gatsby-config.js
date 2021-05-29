91ab8f564946fa355883fe59c5d8940fb0d7ea2c
mikeisfake <mikescooper@icloud.com>
2021-05-29 04:45:34 -0400

    adding manifest back in with animoji as image source for favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/animoji.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-transition-link`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
