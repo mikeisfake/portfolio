f57f870b572eff3e02ee564b17b29d4590d0283b
mikeisfake <mikescooper@icloud.com>
2021-07-17 09:31:43 -0400

    initial rebuild commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,55 +1,26 @@
-import React from 'react'
-import {Helmet} from 'react-helmet'
+import React from "react"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
+import "../styles/index.scss"
 
-import NavLinks from '../components/NavLinks'
-import '../styles/index.scss'
-
-const Layout = ({children}) => {
-
-  const handleBurger = event => {
-    const menu = document.querySelector('.nav-links')
-    const burger = document.querySelector('.hamburger')
-    const container = document.getElementById('main-container')
-    const cover = document.getElementById('cover')
-
-    menu.classList.toggle('open')
-    burger.classList.toggle('open')
-    container.classList.toggle('open')
-    cover.classList.toggle('open')
-  }
-
+const Layout = ({ children }) => {
   return (
-
-    <div id="main-container">
-      <Helmet>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.11.0/devicon.min.css"></link>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-      </Helmet>
-      <div id="cover" onClick={handleBurger}></div>
-      <div id="nav">
-        <div className="hamburger" onClick={handleBurger}>
-          <span className="line"></span>
-        </div>
-        <NavLinks />
-      </div>
+    <div id="layout">
+      <nav id="main-nav">
+        <ul>
+          <li>
+            <Link to="#about">About</Link>
+          </li>
+          <li>
+            <Link to="#projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="#contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
       {children}
-      <div id="footer">
-        <div className="socials">
-          <ul>
-            <li>
-              <a href='https://github.com/mikeisfake' target="_blank" rel="noreferrer"><i class="devicon-github-original" ></i>
-              </a>
-            </li>
-            <li>
-              <a href='https://www.linkedin.com/in/michael-cooper-259985105/' target="_blank" rel="noreferrer"><i class="devicon-linkedin-plain"></i> </a>
-            </li>
-            <li>
-              <a href='https://vocitates-blog.tumblr.com/' target="_blank" rel="noreferrer"><i class="fab fa-tumblr fa-fw"></i> </a>
-            </li>
-          </ul>
-        </div>
-      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,48 @@
 import * as React from "react"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
-
-import Layout from '../components/Layout'
-import { Emoji } from '../components/Emoji'
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import { Emoji } from "../components/Emoji"
 
 const Index = () => {
   return (
     <Layout>
-      <div id="hero">
-        <div className="title">
-          <h1 className="slide-in-left color-slide">
-            <Emoji description="waving hand" hex="&#x1F44B;&#x1F3FE;" className="wave" />i'm mike</h1>
-          <h2 className="slide-in-left delay">& I make beautiful things for the web.</h2>
-        </div>
-        <span className="cta slide-in-left delay-1">
-          <AniLink cover bg="#2f968a" direction="right" to='/contact' className="pulse">contact me</AniLink>
+      <section id="hero">
+        <h3>my name is</h3>
+        <h1>Mike Cooper</h1>
+        <h4>and i make beautiful things for the web.</h4>
+        <p>
+          I'm a front end web developer with a passion for making beautiful and
+          funcional user interfaces to create delightful experences on the web.
+        </p>
+        <span className="cta">
+          <Link to="#contact">contact me.</Link>
         </span>
+      </section>
 
-      </div>
+      <section id="about">
+        <h3>About Me</h3>
+        <p>
+          My name is Mike. I am a frontend software engineer based in the DMV
+          area. I have been writing HTML, CSS and Javascript pages as a hobby
+          since I was 12 years old... I had a really cool myspace, and I created
+          a ton of themes for tumblr back in the day too!.
+        </p>
+        <p>
+          Now, 10 years on, I have decided to make my hobby into a career. I
+          have formalized my casual experience into an education at Flatiron
+          School, where I expanded on my foundational knowledge and acquired a
+          deeper love and understanding of software engineering.
+        </p>
+        <p>
+          I am currently seeking my first full-time real-life job as a developer
+          to start my new career. If you think I might be a good fit for your
+          team please get in touch.
+        </p>
+      </section>
 
+      <section id="projects"></section>
+
+      <section id="contact"></section>
     </Layout>
   )
 }
