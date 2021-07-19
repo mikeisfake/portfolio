493d26bcddd47cec98308a337574233c3687319d
mikeisfake <mikescooper@icloud.com>
2021-07-19 13:26:22 -0400

    updating data with new project and differen fields

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,46 +1,42 @@
 export const ProjectData = [
   {
-    title: 'FILMr',
-    description: 'FILMr is a social application designed for film lovers built with Ruby on Rails. I also created totally custom styles with Sass. No CSS libraries needed. It queries a third party API to return JSON data about various films a user may search for. It then allows users to create reviews for those films and reviews written by other users. I used a ruby gem call acts-as-followers to allow users to follow each other. This is done with a polymorphic association on the backend.',
-    github: 'https://github.com/mikeisfake/filmr',
+    title: "Sims Builds",
+    description:
+      "I used a variety of technologies and tools including Next.js, Sass, Contentful and Cloudinary to create a site to showcase my second favorite hobby: building houses in The Sims. I focused on creating a beautiful UI while maintaining a DRY codebase.",
+    github: "https://github.com/mikeisfake/sims-builds",
+    site: "https://simsbuilds.netlify.app/",
+    image: "/projects/sims-builds.png",
+    technologies: ["Next.js", "Contentful", "Cloudinary", "Sass"],
+  },
+  {
+    title: "FILMr",
+    description:
+      "FILMr is a social application designed for film lovers built with Ruby on Rails. I also created totally custom styles with Sass. No CSS libraries needed. It queries a third party API to return JSON data about various films a user may search for. It then allows users to create reviews for those films and reviews written by other users. I used a ruby gem call acts-as-followers to allow users to follow each other. This is done with a polymorphic association on the backend.",
+    github: "https://github.com/mikeisfake/filmr",
     video: "https://www.youtube.com/embed/se5ysRHcb_A",
-    technologies: [
-      'Ruby on Rails','jQuery','Sass','HTML'
-    ]
+    image: "/projects/filmr.png",
+    technologies: ["Ruby on Rails", "jQuery", "Sass", "erb", "OMDb API"],
   },
 
   {
-    title: 'THIRTYFIVE',
-    description: 'THIRTYFIVE is an application for forgetful film lovers built using React and Redux for the frontend and Ruby on Rails for the backend. This app employs the use of a third party API to fetch data about movies as well as a custom Rails API for managing application data.',
-    github: 'https://github.com/mikeisfake/thirtyfive',
-    video: 'https://www.youtube.com/embed/plAryFUoAfM',
-    technologies: ['React','Redux','CSS','Ruby on Rails'
-    ]
+    title: "THIRTYFIVE",
+    description:
+      "THIRTYFIVE is an application for forgetful film lovers built using React and Redux for the frontend and Ruby on Rails for the backend. This app employs the use of a third party API to fetch data about movies as well as a custom Rails API for managing application data.",
+    github: "https://github.com/mikeisfake/thirtyfive",
+    video: "https://www.youtube.com/embed/plAryFUoAfM",
+    image: "/projects/thirtyfive.png",
+    technologies: ["React", "Redux", "CSS", "Ruby on Rails", "OMDb API"],
   },
-
-  {
-    title: 'm_movies',
-    description: 'Another movies and reviews app with basic CRUD actions. Created with Sinatra and styled with vanilla CSS',
-    github: 'https://github.com/mikeisfake/mikes_movies',
-    video: 'https://www.youtube.com/embed/Kjyp47OqbOc',
-    technologies: ['Ruby', 'Sinatra', 'CSS']
-  },
-
-  {
-   title: 'Vocitates',
-   description: 'Vocitates is a tumblr page I created to store and share the tumblr themes I have created. It was written in HTML, CSS and a little bit of JavaScript. ',
-   github: 'https://github.com/mikeisfake/vocitates',
-   video: 'https://www.youtube.com/embed/WOs5EnHEDV8',
-   technologies: ['HTML','CSS', 'JavaScript', 'jQuery']
- },
 ]
 
 // Data Structure
-//
+
 // {
 //   title: '',
 //   description: '',
 //   github: '',
 //   video: '',
+//   site: '',
+//   image: '',
 //   technologies: []
 // },
