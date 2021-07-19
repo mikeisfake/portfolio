ced479e208219e7ce46b9cbfdc376f5f5b4dbc48
mikeisfake <mikescooper@icloud.com>
2021-07-19 13:24:09 -0400

    add custom cursor implement animate.css set up smooth scrolling and scroll to top

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,83 @@
-import React from "react"
-import { Link } from "gatsby"
-import { Helmet } from "react-helmet"
+import React, { useState, useEffect } from "react";
+import { Link } from 'gatsby';
+import { StaticImage } from "gatsby-plugin-image";
+import { Helmet } from "react-helmet";
 
-import "../styles/index.scss"
+import "../styles/index.scss";
 
 const Layout = ({ children }) => {
+
+  const [left, setLeft] = useState(0)
+  const [top, setTop] = useState(0)
+
+  const handleClick = e => {
+    let el = e.nativeEvent.target.hash;
+    let node = document.querySelector(el)
+    e.preventDefault();
+    node && node.scrollIntoView({ behavior: "smooth", block: "start" })
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  const handleMouseMove = e => {
+    setLeft(e.nativeEvent.pageX)
+    setTop(e.nativeEvent.pageY)
+  }
+
+  const cursorStyle = {
+    top: top + "px",
+    left: left + "px",
+  }
+
   return (
-    <div id="layout">
-      <nav id="main-nav">
-        <ul>
-          <li>
-            <Link to="#about">About</Link>
-          </li>
-          <li>
-            <Link to="#projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="#contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      {children}
-    </div>
+    <>
+      <Helmet>
+        <title>Mike Cooper | Front End Developer</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+      </Helmet>
+      <div id="layout" onMouseMove={handleMouseMove}>
+        <div className="cursor" style={cursorStyle}></div>
+        <nav id="main-nav">
+          <div
+            onClick={scrollToTop}
+            className="img-wrapper animate__animated animate__fadeInUp"
+          >
+            <StaticImage src="../images/animoji.png" alt="animoji protrait" />
+          </div>
+
+          <div className="spacer"></div>
+          <ul>
+            <li className="animate__animated animate__fadeInUp delay">
+              <a href="#about" onClick={handleClick}>
+                About
+              </a>
+            </li>
+            <li className="animate__animated animate__fadeInUp delay-1">
+              <a href="#projects" onClick={handleClick}>
+                Projects
+              </a>
+            </li>
+            <li className="animate__animated animate__fadeInUp delay-2">
+              <a href="#contact" onClick={handleClick}>
+                Contact
+              </a>
+            </li>
+            <li className="animate__animated animate__fadeInUp delay-3">
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </div>
+    </>
   )
 }
 
