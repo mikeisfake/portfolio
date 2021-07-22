105a401a10cc03bf2fb29a00e798a49769e952ce
mikeisfake <mikescooper@icloud.com>
2021-07-22 19:40:04 -0400

    add fade in

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,37 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import Fade from "react-reveal/Fade"
+
 import { Hero } from "../sections/Hero"
 import { About } from "../sections/About"
-import { Contact } from "../sections/Contact"
 import { Projects } from "../sections/Projects"
+import { Contact } from "../sections/Contact"
 
 import Layout from "../components/Layout"
-import { Emoji } from "../components/Emoji"
 
 import { ProjectData as data } from "../data/ProjectData"
 
 const Index = () => {
-
   return (
     <Layout>
-      <section id="hero">
-        <Hero />
-      </section>
+        <section id="hero">
+          <Hero />
+        </section>
 
-      <section id="about">
-        <About />
-      </section>
+      <Fade bottom>
+        <section id="about">
+          <About />
+        </section>
 
-      <section id="projects">
-        <Projects data={data} />
-      </section>
+        <section id="projects">
+          <Projects data={data} />
+        </section>
 
-      <section id="contact">
-        <Contact />
-      </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </Fade>
     </Layout>
   )
 }
